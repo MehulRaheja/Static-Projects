3356bf2bd0ebdefc8d117770100e76bd04318479
MehulMetaversity <mehul@metaversitytechnologies.com>
2023-08-06 01:48:56 +0530

    clipboard project done

---
+++ tailwind-website-projects/clipboard/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], // here we specify where and what kind of files to look for tailwind classes
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+

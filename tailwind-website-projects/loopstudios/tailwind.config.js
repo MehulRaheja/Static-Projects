d3479d49e4fa0aad3032048211910838f138e151
MehulMetaversity <mehul@metaversitytechnologies.com>
2023-08-06 22:42:36 +0530

    project loopstudio continue

--- tailwind-website-projects/loopstudios/tailwind.config.js
+++ tailwind-website-projects/loopstudios/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./*.html'], // here we specify where and what kind of files to look for tailwind classes
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'san-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em'
+      }
+    },
   },
   plugins: [],
 }
